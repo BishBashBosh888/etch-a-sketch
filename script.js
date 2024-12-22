24a424835489675f97c5e6a93c6782f6226b4e10
fsut0005 <fsut0005@student.monash.edu>
2024-12-22 23:46:24 +1100

    made 16*16 tiles using javascript
    added css to make it appear as 16x16 tiles

---
+++ script.js
@@ -0,0 +1,10 @@
+const board = document.querySelector(".board");
+
+for(let i = 0; i < 16*16; i++){
+    const div = document.createElement("div");
+    board.appendChild(div);
+    div.textContent = i+1;
+    div.setAttribute("id", `ID${i+1}`);
+    div.classList.add("tile");
+}
+
