28141960c9379d1d94489b6c59dad825aaa2a61e
fsut0005 <fsut0005@student.monash.edu>
2024-12-25 10:38:51 +1100

    added mouseover and mouseout effect on each tiles

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ for(let i = 0; i < 16*16; i++){
     div.textContent = i+1;
     div.setAttribute("id", `ID${i+1}`);
     div.classList.add("tile");
+    
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "gray";
+    })
+
+    div.addEventListener("mouseout" ,() => {
+        div.style.backgroundColor = "white";
+    })
 }
+
 
