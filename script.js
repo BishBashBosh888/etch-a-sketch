ceb71c8ed52b2796205e0521ca24e21a8c748c5d
fsut0005 <fsut0005@student.monash.edu>
2024-12-26 11:47:16 +1100

    added options to change board dimension

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 const board = document.querySelector(".board");
 
-for(let i = 0; i < 16*16; i++){
-    const div = document.createElement("div");
-    board.appendChild(div);
-    div.textContent = i+1;
-    div.setAttribute("id", `ID${i+1}`);
-    div.classList.add("tile");
-    
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "gray";
-    })
+board.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("tile")) {
+        event.target.style.backgroundColor = "gray";
+    }
+})
 
-    div.addEventListener("mouseout" ,() => {
-        div.style.backgroundColor = "white";
-    })
+board.addEventListener("mouseout" ,(event) => {
+    if (event.target.classList.contains("tile")) {
+        event.target.style.backgroundColor = "white";
+    }
+})
+
+function generateTiles(size){
+    board.innerHTML = "";
+
+    const tileSize = 960 / size;
+
+    for(let i = 0; i < size*size; i++){
+        const div = document.createElement("div");
+        div.setAttribute("id", `ID${i+1}`);
+        div.classList.add("tile");
+        div.style.width = `${tileSize}px`
+        div.style.height = `${tileSize}px`
+
+        div.addEventListener("mouseover", () =>{
+            div.style.backGround = "gray";
+        })
+
+        div.addEventListener("mouseout", () =>{
+            div.style.backGround = "white";
+        })
+
+        board.appendChild(div);
+    }
 }
+
+generateTiles(16);
 
 
